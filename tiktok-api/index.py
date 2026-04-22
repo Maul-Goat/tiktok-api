a89e5cf0b65cf3b5476812ed4125add89ec9fc8e
Maul-Goat <achmadmaulanaxx@gmail.com>
2026-04-22 23:28:22 +0700

    Update index.py

--- tiktok-api/index.py
+++ tiktok-api/index.py
@@ -82,5 +82,4 @@ def analyze_user(username: str):
         return {"success": False, "error": str(e)}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
